87d93801fd84d1a9c8d3b858c2d835e1076be8b3
Etienne Rossignon <etienne.rossignon@gadz.org>
2013-06-15 23:57:55 +0200

    * add STEPtoBREP command

---
+++ bin/STEPtoBREP.cmd
@@ -0,0 +1,1 @@
+node STEPtoBREP.js %1 %2 %3 %4 %5 %6
---
+++ bin/STEPtoBREP.js
@@ -0,0 +1,26 @@
+
+var occ = require('../lib/occ')
+
+var pace = require("pace")(1000)
+
+filename = process.argv[2];
+
+occ.readSTEP(filename,function (err,solids) {
+   pace.op(100000)
+   if (!err) {
+      console.log( " \n\ndone y----> ", solids.length , " solids read");
+
+      // write to BREP
+      occ.writeBREP(filename+".brep",solids,function done() { });
+   } else {
+      message = solids
+      console.log("\n\n ERROR =" , message);
+   }
+   
+}, function progress(percent,incr) { 
+ pace.op(percent*100*10)
+ // console.log("tick ", incr,bar1.step)
+ //  bar1.tick(incr);
+});
+
+console.log(process.argv[0]);
