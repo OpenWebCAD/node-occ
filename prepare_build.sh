51d0df499179e6468006d4d32c06c3d9cc8ba090
Etienne <etienne.rossignon@gadz.org>
2014-05-07 21:50:27 +0200

    Update prepare_build.sh

--- prepare_build.sh
+++ prepare_build.sh
@@ -1,6 +1,10 @@
 grep -i VERSION /usr/include/oce/Standard_Version.hxx ;
 lscpu ;
 cmake --version ;
+
+set NPROC=`nproc`
+echo "Numbrer of processors =" ${NPROC}
+
 cd oce ;
 git checkout tags/OCE-0.13 ;
 cd .. ;
@@ -10,27 +14,44 @@ cd build_oce ;
 cmake -D OCE_INSTALL_PREFIX:STRING='/usr/local' -D OCE_DRAW:BOOLEAN=FALSE -D OCE_OCAF:BOOLEAN=FALSE -D  OCE_VISUALISATION:BOOLEAN=FALSE -D OCE_DISABLE_X11:BOOLEAN=TRUE -D OCE_USE_PCH:BOOLEAN=TRUE  ../oce > /dev/null;
 
 
-echo 1;
-make -s -j 72 TKernel > /dev/null;
-echo 2;
-make -s -j 72 TKMath > /dev/null;
-echo 3;
-make -s -j 72 TKG2d > /dev/null;
-make -s -j 72 TKGeomBase   > /dev/null;
-make -s -j 72 TKG3d > /dev/null;
-make -s -j  72 TKPrim > /dev/null;
-make -s -j 72 TKBO   > /dev/null;
-make -s -j 72 TKBRep   > /dev/null;
-make -s -j 72 TKXmlL  > /dev/null;
-make -s -j 72 TKShHealing  > /dev/null;
-make -s -j 72 TKPLCAF  > /dev/null;
-make -s -j 72 TKPShape  > /dev/null;
-make -s -j 72 TKSTEPBase  > /dev/null;
-make -s -j 72 TKSTEP209  > /dev/null;
-make -s -j 72 TKSTEPAttr  > /dev/null;
-make -s -j 72 TKIGES  > /dev/null;
-make -s -j 72 TKSTEP  > /dev/null;
-make -s -j 72 ;
+echo 1 `date +%R:%S`;
+make -s -j ${NPROC}  TKernel > /dev/null;
+echo 2 `date +%R:%S`;
+make -s -j ${NPROC} TKMath > /dev/null;
+echo 3 `date +%R:%S`;
+make -s -j ${NPROC} TKG2d > /dev/null;
+echo 4 `date +%R:%S`;
+make -s -j ${NPROC} TKGeomBase   > /dev/null;
+echo 5 `date +%R:%S`;
+make -s -j ${NPROC} TKG3d > /dev/null;
+echo 6 `date +%R:%S`;
+make -s -j ${NPROC} TKPrim > /dev/null;
+echo 7 `date +%R:%S`;
+make -s -j ${NPROC} TKBO   > /dev/null;
+echo 8 `date +%R:%S`;
+make -s -j ${NPROC} TKBRep   > /dev/null;
+echo 9 `date +%R:%S`;
+make -s -j ${NPROC} TKXmlL  > /dev/null;
+echo 10 `date +%R:%S`;
+make -s -j ${NPROC} TKShHealing  > /dev/null;
+echo 11 `date +%R:%S`;
+make -s -j ${NPROC} TKPLCAF  > /dev/null;
+echo 12 `date +%R:%S`;
+make -s -j ${NPROC} TKPShape  > /dev/null;
+echo 13 `date +%R:%S`;
+make -s -j ${NPROC} TKSTEPBase  > /dev/null;
+echo 14 `date +%R:%S`;
+make -s -j ${NPROC} TKSTEP209  > /dev/null;
+echo 15 `date +%R:%S`;
+make -s -j ${NPROC} TKSTEPAttr  > /dev/null;
+echo 16 `date +%R:%S`;
+make -s -j ${NPROC} TKIGES  > /dev/null;
+echo 17 `date +%R:%S`;
+make -s -j ${NPROC} TKSTEP  > /dev/null;
+echo 18 `date +%R:%S`;
+make -s -j ${NPROC} ;
+echo 19 `date +%R:%S`;
+
 sudo make install > /dev/null  ;
 sudo ldconfig ;
 cd ..
