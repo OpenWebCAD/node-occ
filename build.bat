daf930abb1bb8968873a32fb761bb83dd46f55e7
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-03-29 23:30:31 +0200

    build.bat

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ cd build_oce
 cmake -D OCE_INSTALL_PREFIX:STRING='C:/OCE-17' -D OCE_DRAW:BOOLEAN=FALSE -D OCE_OCAF:BOOLEAN=FALSE -D  OCE_VISUALISATION:BOOLEAN=FALSE -D OCE_DISABLE_X11:BOOLEAN=TRUE -D OCE_USE_PCH:BOOLEAN=TRUE  ../oce  
 msbuild oce.sln
 msbuild ocd.sln /p:Configuration=Release
-set VisualStudioVersion=11.0
+set VisualStudioVersion=12.0
 msbuild INSTALL.vcxproj /p:Configuration=Release
 
 cd ..
