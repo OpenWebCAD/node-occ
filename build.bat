7c15033cbcc248c55b029254a0f5478990543121
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-03-30 10:57:44 +0200

    add ability to load a study from server

--- build.bat
+++ build.bat
@@ -1,14 +1,18 @@
-git submodule update --init --recursive
- mkdir build_oce
+CALL git submodule update --init --recursive
+mkdir build_oce
 cd build_oce
-cmake -D OCE_INSTALL_PREFIX:STRING='C:/OCE-17' -D OCE_DRAW:BOOLEAN=FALSE -D OCE_OCAF:BOOLEAN=FALSE -D  OCE_VISUALISATION:BOOLEAN=FALSE -D OCE_DISABLE_X11:BOOLEAN=TRUE -D OCE_USE_PCH:BOOLEAN=TRUE  ../oce  
+SET PREFIX=C:/OCE-17
+
+set GENERATOR=Visual Studio 11 2012
+set VisualStudioVersion=11.0
+CALL cmake -D OCE_INSTALL_PREFIX:STRING="%PREFIX%" -D OCE_DRAW:BOOLEAN=FALSE -D OCE_OCAF:BOOLEAN=FALSE -D  OCE_VISUALISATION:BOOLEAN=FALSE -D OCE_DISABLE_X11:BOOLEAN=TRUE -D OCE_DISABLE_TKSERVICE_FONT:BOOLEAN=TRUE -D OCE_USE_PCH:BOOLEAN=TRUE  -G "%GENERATOR%" ../oce  
 REM msbuild /m oce.sln
-msbuild /m oce.sln /p:Configuration=Release
-set VisualStudioVersion=12.0
-msbuild INSTALL.vcxproj /p:Configuration=Release
+CALL msbuild /m oce.sln /p:Configuration=Release
+
+CALL msbuild INSTALL.vcxproj /p:Configuration=Release
 
 cd ..
-SET CL=/Ic:\OCE-0.17\include\oce
-SET LINK=/LIBPATH:c:\OCE-0.17\Win32\lib
-PATH c:\OCE-0.17\Win32\bin;%PATH%
-npm install
+SET CL=/I%PREFIX%/include/oce
+SET LINK=/LIBPATH:%PREFIX%/Win32/lib
+PATH %PREFIX%/Win32/bin;%PATH%
+CALL npm install
