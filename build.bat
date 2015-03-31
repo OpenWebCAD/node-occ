f3f6c5faab1ad79b28cfa82403dd335e70532a0c
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-03-31 07:47:13 +0200

    test_appveyor

--- build.bat
+++ build.bat
@@ -1,8 +1,13 @@
+ECHO ON
+REM ----------------------------------------------------------
+REM  PREPARE
+REM ----------------------------------------------------------
 CALL git submodule update --init --recursive
+
 CALL SETENV.BAT
+
 mkdir build_oce
 cd build_oce
-
 
 REM set GENERATOR=Visual Studio 11 2012
 REM set VisualStudioVersion=11.0
@@ -26,13 +31,33 @@ CALL cmake -DOCE_INSTALL_PREFIX:STRING="%PREFIX%" ^
 -DOCE_DISABLE_X11:BOOLEAN=ON ^
 -DOCE_DISABLE_TKSERVICE_FONT:BOOLEAN=ON ^
 -DOCE_USE_PCH:BOOLEAN=ON  ^
--G "%GENERATOR%" ../oce  
+-G "%GENERATOR%" ../oce   > nul
+
 
 REM msbuild /m oce.sln
-CALL msbuild /m oce.sln /p:Configuration=Release /verbosity:quiet /consoleloggerparameters:Summary;ShowTimestamp 
+CALL msbuild /m oce.sln /p:Configuration=Release > nul
 
-CALL msbuild /m INSTALL.vcxproj /p:Configuration=Release /verbosity:quiet /consoleloggerparameters:Summary;ShowTimestamp 
+CALL msbuild INSTALL.vcxproj /p:Configuration=Release > nul
 ECHO PREFIX = %PREFIX%
 ECHO PREFIX = %GENERATOR%
 cd ..
+
+REM ----------------------------------------------------------
+REM  BUILD
+REM ----------------------------------------------------------
 CALL npm install
+
+REM ----------------------------------------------------------
+REM  TEST
+REM ----------------------------------------------------------
+CALL npm test
+
+REM ----------------------------------------------------------
+REM  PACKAGE
+REM ----------------------------------------------------------
+SET SRC=%PREFIX%/Win32/bin
+SET SRC=%SRC:/=\%
+XCOPY %SRC%\*.dll .\build\Release
+SET PACKAGE=node-occ-package.zip
+7z a %PACKAGE% .\build\Release\*.*
+appveyor PushArtifact %PACKAGE%
