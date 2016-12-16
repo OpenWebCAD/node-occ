a46cc320bd789f4034cf4065662d91869bf01e80
Victofel <victofel1992@gmail.com>
2016-12-16 17:23:15 +0100

    Angular 2 Sample - First steps
    - New project generate using Angular-cli
    - New viewer component
    - New code editor component

---
+++ sample/angular2/src/app/viewer/viewer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewer',
+  template: `
+  `,
+  styleUrls: ['./viewer.component.css']
+})
+export class ViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+}
